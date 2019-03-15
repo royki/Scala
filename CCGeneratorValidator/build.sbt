63e1b78fca5de894d69d64edada7a6f20b9df403
royki <kearan.rayofsun@gmail.com>
2019-03-16 00:46:56 +0100

    update scala version

--- CCGeneratorValidator/build.sbt
+++ CCGeneratorValidator/build.sbt
@@ -1,6 +1,6 @@
 version  := "0.0.1"
 organization := "com"
-scalaVersion := "2.12.6"
+scalaVersion := "2.12.8"
 triggeredMessage := Watched.clearWhenTriggered
 
 libraryDependencies ++= Seq(
--- CCGeneratorValidator/src/main/scala/CC.scala
+++ CCGeneratorValidator/src/main/scala/CC.scala
@@ -1,10 +1,11 @@
-sealed trait CC extends Any with Product with Serializable{
+// Sealed trait
+sealed trait CC extends Any with Product with Serializable {
 	import CC._
 
 	def number: String
 
+	// Companion method
 	final def isValid: Boolean = isInstanceOf[Valid]
-
 	final def isNotValid: Boolean = !isValid
 
 	final override def toString: String = {
@@ -20,29 +21,30 @@ sealed trait CC extends Any with Product with Serializable{
 	}
 }
 
+// Companion object
 object CC extends (String => CC) with (() => CC) {
 
+
+	final case class Invalid private (number: String) extends AnyVal with CC /*CC*/
 	object Invalid {
 		private[CC] def apply(number: String): Invalid = {
 			new Invalid(number)
 		}
 	}
 
+	final case class Valid private (number: String) extends AnyVal with CC /*CC*/
 	object Valid {
 		private[CC] def apply(number: String): Valid = {
 			new Valid(number)
 		}
 	}
 
-	final case class Invalid private (number: String) extends AnyVal with CC /*CC*/ 
-	final case class Valid private (number: String) extends AnyVal with CC /*CC*/
-
 	def apply(number: String): CC = {
 		if(isValid(number))
 			Valid(number)
 		else Invalid(number)
 	}
-	
+
 	private val checkDigitLength = 1
 	private val MinimumLength = 13
 	private val MaximumLength = 19
@@ -64,59 +66,60 @@ object CC extends (String => CC) with (() => CC) {
 
 	private def luhn(payload: String): Int = {
 		payload
-			.reverse // String
-			.map(_.toString.toInt) // IndexedSeq[Int]
-			.zipWithIndex // IndexedSeq[(Int= Digit, Int= ZeroBasedIndex)] // 1st element has index zero
-			.map {
-				case (digit, index) => 
-					if(index % 2 == 0)
-						digit * 2
-					else 
-						digit
-			} // IndexedSeq[Int]			
+		.reverse // String
+		.map(_.toString.toInt) // IndexedSeq[Int]
+		.zipWithIndex // IndexedSeq[(Int= Digit, Int= ZeroBasedIndex)] // 1st element has index zero
+		.map {
+			case (digit, index) =>
+				if(index % 2 == 0)
+					digit * 2
+				else
+					digit
+			} // IndexedSeq[Int]
 			.map {
 				number => if(number > 9) number - 9
-						  else number
+				else number
 			} // IndexedSeq[Int = Digit]
 			.sum
-	}
-
-	def apply(): Valid = {
-		Valid(generatedNumber)	
-	}	
-
-	private def split(number: String):(String, Int) = {
-		val payload = number.dropRight(checkDigitLength)
-		val checkDigit = number.takeRight(checkDigitLength).toInt
-		// payload -> checkDigit
-		(payload, checkDigit)
-	}
-
-	private def generatedNumber: String = {
-		val payload = {
-			import scala.util.Random
-			val min: Int = MinimumLength - checkDigitLength // 12
-			val max: Int = MaximumLength - checkDigitLength // 18
-			val length: Int = min + Random.nextInt((max - min) + 1) // 12 to 18 			
-			
-			def randomDigit: Int = 
-				Random.nextInt(10) // 0 to 9
-
-			(1 to length) // FiniteRange
-				.map(_ => randomDigit) // IndexedSeq[Int]
-				.mkString // String
 		}
 
-		val checkDigit: Int = 
-			(10 - (luhn(payload) % 10)) % 10
-		
-		val number: String = payload + checkDigit
+		// Generate Valid number
+		def apply(): Valid = {
+			Valid(generatedNumber)
+		}
 
-		if(isValid(number))
-			number
-		else
-			// $COVERAGE-OFF$
+		private def split(number: String):(String, Int) = {
+			val payload = number.dropRight(checkDigitLength)
+			val checkDigit = number.takeRight(checkDigitLength).toInt
+			// payload -> checkDigit
+			(payload, checkDigit)
+		}
+
+		private def generatedNumber: String = {
+			val payload = {
+				import scala.util.Random
+				val min: Int = MinimumLength - checkDigitLength // 12
+				val max: Int = MaximumLength - checkDigitLength // 18
+				val length: Int = min + Random.nextInt((max - min) + 1) // 12 to 18
+				//    12  + 0 until (6 + 1)
+				def randomDigit: Int =
+					Random.nextInt(10) // 0 to 9
+
+				(1 to length) // FiniteRange
+				.map(_ => randomDigit) // IndexedSeq[Int]
+				.mkString // String
+			}
+
+			val checkDigit: Int =
+				(10 - (luhn(payload) % 10)) % 10
+
+			val number: String = payload + checkDigit
+
+			if(isValid(number))
+				number
+			else
+				// $COVERAGE-OFF$
 			sys.error(s"Bug: Generated an Invalid Number: $number")
 			// $COVERAGE-OFF$
+		}
 	}
-}
