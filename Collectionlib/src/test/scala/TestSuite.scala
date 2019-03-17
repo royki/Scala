25a6b6b3fc02920e46dd30b539a03f37652fe25a
royki <kearan.rayofsun@gmail.com>
2019-03-17 20:04:40 +0100

    Set Collection and SetSuite

---
+++ Collectionlib/src/test/scala/TestSuite.scala
@@ -0,0 +1,34 @@
+import org.scalatest._
+
+/*class Suite extends FunSuite with Matchers {
+
+  test("Test Success") {
+    assert(true)
+    assert(Set.empty.size == 0)
+    Set.empty.size.shouldBe(0)
+
+    "Hello World" should include("He")
+    "Hello World" should not include ("Hi")
+    Set.empty.isEmpty shouldBe true
+    Seq(1, 2, 4) should not be 'empty
+    Seq(1, 2, 4).isEmpty shouldBe false
+  }
+
+  test("Test Failure") {
+    println("Second One")
+    throw new Exception
+    assert(false)
+  }
+
+  ignore("Ignore Test") {
+    throw new Exception
+  }
+
+  test("Pending Tests") {
+    pending
+    throw new Exception
+  }
+
+}
+
+*/ 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# SBT Scala Project
+#### _Scala Homegrown Collecton Librabry_
+-----------------------------------------
+
+- Following from [DevInsideYou](https://www.youtube.com/watch?v=g-0inzMUsRg&list=PLJGDHERh23x-4bTASKbtwhhAuP6rYQJqE)
