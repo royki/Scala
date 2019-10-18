0f13fe336bc65698c44ac72f66c0bdf34a27301d
kroy <kroy@vulog.com>
2019-10-18 16:08:02 +0200

    Set in OOP with Scala

---
+++ Collectionlib/src/main/scala/collections/SetOop.scala
@@ -0,0 +1,149 @@
+package collections
+
+sealed trait SetOop extends (String => Boolean) {
+  /*
+  From here, we change implementation approach a little (differ from OldSet.scala). We ll follow now OOP approach,
+  We ll have add as `abstract` method and two different implementation of `add`;
+  One for Empty Set and another for NonEmpty Set
+  We make the `add` function abstract here
+  */
+
+  def add(input: String): SetOop
+  def remove(input: String): SetOop
+  def union(that: SetOop): SetOop
+  def intersect(that: SetOop): SetOop
+  def diff(that: SetOop): SetOop
+
+  def isSubsetOf(that: SetOop): Boolean 
+  final def isSupersetOf(that: SetOop): Boolean = that.isSubsetOf(this)
+  
+  final override def equals(other: Any): Boolean = other match {
+    case that: SetOop => this.isSubsetOf(that) & that.isSubsetOf(this)
+    case _ => false
+  }
+  def size: Int
+  def isEmpty: Boolean /* = this eq Set.empty */
+  final def nonEmpty: Boolean = !isEmpty
+
+  def isSingleton: Boolean
+
+  def sample: Option[String]
+
+}
+// Companion object of the trait
+object SetOop {
+
+  // ----------------Implementation of NonEmpty Set -------------------------//
+  /*
+  There is a convention in FP, it is called Cons or Constructed, here it is named as Link or NonEmpty Set
+  empty Set provides the implementation of `apply` [`(String => Boolean)]`
+  So we need to implement it for NonEmpty Set or Link
+  */
+
+  // We ll have a case class of `NonEmpty` Set, that ll extend Trait Set
+  private final case class NonEmpty(element: String, otherElements: SetOop) extends SetOop {
+    override final def apply(input: String): Boolean =
+      input == element || otherElements(input)
+
+    // Add in NonEmpty Set
+    override final def add(input: String): SetOop =
+      if (input == element)
+        this
+      else
+        NonEmpty(input, otherElements.add(element))
+
+    // Remove in NonEmpty Set
+    override final def remove(input: String): SetOop =
+      if (input == element) otherElements
+      else NonEmpty(element, otherElements.remove(input))
+
+    // Union in NonEmpty Set
+    override final def union(that: SetOop): SetOop = {
+      val newSet = that.add(element)
+      otherElements.union(newSet)
+    }
+    
+    // Intersection in NonEmpty Set
+    override final def intersect(that: SetOop): SetOop = {
+      if(that(element))
+      otherElements.intersect(that).add(element)
+      else
+      otherElements.intersect(that)
+    }
+    
+    // Difference in NonEmpty Set
+    override final def diff(that: SetOop): SetOop = {
+      if(that(element))
+        otherElements.diff(that)
+      else
+        otherElements.diff(that).add(element)
+      }
+
+    // isSubsetOf NonEmpty Set
+    override final def isSubsetOf(that: SetOop): Boolean = 
+      that(element) && otherElements.isSubsetOf(that)
+
+    // hashCode
+    override final def hashCode: Int = element.hashCode + otherElements.hashCode  
+
+    // Size of NonEmpty Set
+    override final def size: Int = 1 + otherElements.size
+
+    // isEmpty on NonEmpty Set
+    override final def isEmpty: Boolean = false
+    
+    // isSingleton on NonEmpty Set
+    override final def isSingleton: Boolean = otherElements.isEmpty
+
+    // Sample of NonEmpty Set
+    override final def sample: Option[String] = Some(element)
+  }
+
+  // ----------------Implementation of Empty Set -------------------------//
+
+  // We ll have a private object named `Empty`, that ll extend Trait Set
+  private object Empty extends SetOop {
+    def apply(input: String): Boolean = false
+
+    /*
+    Add in Empty Set
+    that is actually the production of NonEmpty Sets. As soon as, you add something in the NonEmpty Set, it becomes a nonempty set
+   */
+    override final def add(input: String): SetOop = NonEmpty(input, Empty)
+
+    // Remove in Empty Set
+    override final def remove(input: String): SetOop = this
+
+    // Union in Empty Set
+    override final def union(that: SetOop): SetOop = that
+
+    // Intersection in NonEmpty Set
+    override final def intersect(that: SetOop): SetOop = this
+
+    // Difference in NonEmpty Set
+    override final def diff(that: SetOop): SetOop = this
+
+    // isSubsetOf NonEmpty Set
+    override final def isSubsetOf(that: SetOop): Boolean = true
+
+    // Size of Empty Set
+    override final def size: Int = 0
+
+    // isEmpty on Empty Set
+    override final def isEmpty: Boolean = true
+
+    // isSingleton on Empty Set
+    override final def isSingleton: Boolean = false
+
+    // Sample of Empty Set
+    override final def sample: Option[String] = None
+  }
+
+  /*
+  Empty Set provides the implementation of `(String => Boolean)`
+  val empty: Set = input => false
+  As we have an object named `Empty` which has same `apply` implementation, we need to use it for Set [`val empty: OldSet = input => false`]
+   */
+  val empty: SetOop = Empty
+
+}
--- Collectionlib/src/test/scala/SetSuite.scala
+++ Collectionlib/src/test/scala/SetSuite.scala
@@ -8,16 +8,18 @@ class SetSuite extends FunSuite with Matchers {
     scala.util.Random.alphanumeric.take(5).mkString
 
   test("apply on an empty Set should yield false") {
-    Set.empty(randomString) shouldBe false
+    SetOop.empty(randomString) shouldBe false 
+    SetOop.empty.size shouldBe 0
   }
 
+  // Test add element of Set
   test("add on an empty Set should yield a new Set with one element") {
     val first = randomString
     val second = randomString
 
     first should not be second
 
-    val set = Set.empty.add(first)
+    val set = SetOop.empty.add(first)
 
     set(first) shouldBe true
     set(second) shouldBe false
@@ -29,22 +31,23 @@ class SetSuite extends FunSuite with Matchers {
 
     first should not be second
 
-    val set = Set.empty.add(first).add(second)
+    val set = SetOop.empty.add(first).add(second)
 
     set(first) shouldBe true
     set(second) shouldBe true
   }
 
+  // Test remove element of Set
   test("remove on an empty Set should yield an empty Set") {
     val element = randomString
-    val stillEmpty = Set.empty.remove(element)
+    val stillEmpty = SetOop.empty.remove(element)
     stillEmpty(element) shouldBe false
   }
 
   test("remove on a nonEmpty Set should yield a new Set without element") {
     val element = randomString
 
-    val setWithElement = Set.empty.add(element)
+    val setWithElement = SetOop.empty.add(element)
 
     setWithElement(element) shouldBe true
 
@@ -57,7 +60,7 @@ class SetSuite extends FunSuite with Matchers {
     val first = randomString
     val second = randomString
 
-    val setWithElement = Set.empty.add(first).add(second)
+    val setWithElement = SetOop.empty.add(first).add(second)
 
     setWithElement(first) shouldBe true
     setWithElement(second) shouldBe true
@@ -72,7 +75,7 @@ class SetSuite extends FunSuite with Matchers {
     val first = randomString
     val second = randomString
 
-    val setWithElement = Set.empty.add(first).add(second)
+    val setWithElement = SetOop.empty.add(first).add(second)
 
     setWithElement(first) shouldBe true
     setWithElement(second) shouldBe true
@@ -85,144 +88,317 @@ class SetSuite extends FunSuite with Matchers {
 
   test("add/remove combo should ensure that all elements are distinct") {
     val element = randomString
-    val set = Set.empty.add(element).add(element).remove(element)
+    val set = SetOop.empty.add(element).add(element).remove(element)
     set(element) shouldBe false
   }
 
-  // test("union on empty Set should yield an empty Set") {
-  //   Set.empty.union(Set.empty)(randomString) shouldBe false
-  // }
+  // Test uninon element of Set
+  test("union on empty Set should yield an empty Set") {
+    SetOop.empty.union(SetOop.empty)(randomString) shouldBe false
+    
+    // Test Equality on Set
+    val empty = SetOop.empty
+    val uninon = empty.union(empty)
 
-  // test("union on non empty Set with an empty Set should yield the original Set untouched") {
-  //   // val nonEmptySet = Set.empty.add(randomString)
-  //   // Set.empty.union(nonEmptySet) shouldBe true
+    empty.isSubsetOf(uninon) shouldBe true
+    uninon.isSubsetOf(empty) shouldBe true
 
-  //   val first = randomString
-  //   val second = randomString
-
-  //   first should not be second
-
-  //   val emptySet = Set.empty
-  //   val nonEmptySet1 = emptySet.add(first).add(second)
-
-  //   emptySet.union(nonEmptySet1)(first) shouldBe true
-  //   emptySet.union(nonEmptySet1)(second) shouldBe true
-
-  //   nonEmptySet1.union(emptySet)(first) shouldBe true
-  //   nonEmptySet1.union(emptySet)(second) shouldBe true
-  // }
-
-  // test("union on two non empty Set with an empty Set should yield their union") {
-  //   val a = randomString
-  //   val b = randomString
-  //   val c = randomString
-  //   val d = randomString
-
-  //   val left = Set.empty.add(a).add(b)
-  //   val right = Set.empty.add(c).add(d)
-
-  //   left.union(right)(a) shouldBe true
-  //   left.union(right)(b) shouldBe true
-  //   left.union(right)(c) shouldBe true
-  //   left.union(right)(d) shouldBe true
-  // }
-
-  // test("intersection on empty Set should yield an empty Set") {
-  //   // Set.empty.intersect(Set.empty) shouldBe Set.empty // this is failing as there is no equality function
-  //   Set.empty.intersect(Set.empty)(randomString) shouldBe false
-  // }
-
-  // test("intersection on non empty Set with an empty Set should yield the original Set untouched") {
-  //   val first = randomString
-  //   val second = randomString
-
-  //   first should not be second
-
-  //   val emptySet = Set.empty
-  //   val NonEmptySet = emptySet.add(first).add(second)
-
-  //   emptySet.intersect(NonEmptySet)(first) shouldBe false
-  //   emptySet.intersect(NonEmptySet)(second) shouldBe false
-
-  //   NonEmptySet.intersect(emptySet)(first) shouldBe false
-  //   NonEmptySet.intersect(emptySet)(second) shouldBe false
-  // }
-
-  // test("intersection on two non empty Set with an empty Set should yield their common Set") {
-  //   val a = randomString
-  //   val b = randomString
-  //   val c = randomString
-  //   val d = randomString
-  //   val e = randomString
-
-  //   val left = Set.empty.add(a).add(b).add(c)
-  //   val right = Set.empty.add(c).add(d).add(e)
-
-  //   val intersectionLeft = left.intersect(right)
-
-  //   intersectionLeft(a) shouldBe false
-  //   intersectionLeft(b) shouldBe false
-  //   intersectionLeft(c) shouldBe true
-  //   intersectionLeft(d) shouldBe false
-  //   intersectionLeft(e) shouldBe false
-
-  //   val intersectionRight = right.intersect(left)
-
-  //   intersectionRight(a) shouldBe false
-  //   intersectionRight(b) shouldBe false
-  //   intersectionRight(c) shouldBe true
-  //   intersectionRight(d) shouldBe false
-  //   intersectionRight(e) shouldBe false
-  // }
-
-  // test("difference on empty Set should yield an empty Set") {
-  //   Set.empty.diff(Set.empty)(randomString) shouldBe false
-  // }
-
-  // test("difference on a non empty Set with an empty Set should yield an empty Set") {
-  //   val first = randomString
-  //   val second = randomString
-
-  //   first should not be second
-
-  //   val emptySet = Set.empty
-  //   val nonEmptySet = emptySet.add(first).add(second)
-
-  //   emptySet.diff(nonEmptySet)(first) shouldBe false
-  //   emptySet.diff(nonEmptySet)(second) shouldBe false
-
-  //   nonEmptySet.diff(emptySet)(first) shouldBe true
-  //   nonEmptySet.diff(emptySet)(second) shouldBe true
-  // }
-
-  // test("difference on two non empty Sets should yield their difference") {
-  //   val a = randomString
-  //   val b = randomString
-  //   val c = randomString
-  //   val d = randomString
-
-  //   val left = Set.empty.add(a).add(b).add(c)
-  //   val right = Set.empty.add(b).add(c).add(d)
-
-  //   val leftDifference = left.diff(right)
-
-  //   leftDifference(a) shouldBe true
-  //   leftDifference(b) shouldBe false
-  //   leftDifference(c) shouldBe false
-  //   leftDifference(d) shouldBe false
-
-  //   val rightDifference = right.diff(left)
-
-  //   rightDifference(a) shouldBe false
-  //   rightDifference(b) shouldBe false
-  //   rightDifference(c) shouldBe false
-  //   rightDifference(d) shouldBe true
-  // }
-
-  test("isSubsetOf on an empty Set should yield true") {
-    pending
-
-    Set.empty.isSubsetOf(Set.empty) shouldBe true
-    Set.empty.isSubsetOf(Set.empty.add(randomString)) shouldBe true
+    SetOop.empty.union(SetOop.empty) shouldBe SetOop.empty
   }
+
+  test("union on non empty Set with an empty Set should yield the original Set untouched") {
+    // val nonEmptySet = SetOop.empty.add(randomString)
+    // SetOop.empty.union(nonEmptySet) shouldBe true
+
+    val first = randomString
+    val second = randomString
+
+    first should not be second
+
+    val emptySet = SetOop.empty
+    val nonEmptySet1 = emptySet.add(first).add(second)
+
+    emptySet.union(nonEmptySet1)(first) shouldBe true
+    emptySet.union(nonEmptySet1)(second) shouldBe true
+
+    nonEmptySet1.union(emptySet)(first) shouldBe true
+    nonEmptySet1.union(emptySet)(second) shouldBe true
+  }
+
+  test("union on two non empty Set with an empty Set should yield their union") {
+    val a = randomString
+    val b = randomString
+    val c = randomString
+    val d = randomString
+
+    val left = SetOop.empty.add(a).add(b)
+    val right = SetOop.empty.add(c).add(d)
+
+    // As we have the equality function, so we can test the using equality function
+    /* 
+    left.union(right)(a) shouldBe true
+    left.union(right)(b) shouldBe true
+    left.union(right)(c) shouldBe true
+    left.union(right)(d) shouldBe true
+
+    right.union(left)(a) shouldBe true
+    right.union(left)(b) shouldBe true
+    right.union(left)(c) shouldBe true
+    right.union(left)(d) shouldBe true 
+    */
+
+    
+    // Test with equality on Set
+    left.union(right) shouldBe SetOop.empty.add(a).add(b).add(c).add(d)
+    right.union(left) shouldBe SetOop.empty.add(a).add(b).add(c).add(d)
+
+
+  }
+
+  // Test intersection element of Set
+  test("intersection on empty Set should yield an empty Set") {
+    // Set.empty.intersect(Set.empty) shouldBe Set.empty // this is failing as there is no equality function
+    SetOop.empty.intersect(SetOop.empty)(randomString) shouldBe false
+  }
+
+  test("intersection on non empty Set with an empty Set should yield the original Set untouched") {
+    val first = randomString
+    val second = randomString
+
+    first should not be second
+
+    val emptySet = SetOop.empty
+    val NonEmptySet = emptySet.add(first).add(second)
+
+    emptySet.intersect(NonEmptySet)(first) shouldBe false
+    emptySet.intersect(NonEmptySet)(second) shouldBe false
+
+    NonEmptySet.intersect(emptySet)(first) shouldBe false
+    NonEmptySet.intersect(emptySet)(second) shouldBe false
+  }
+
+  test("intersection on two non empty Set with an empty Set should yield their common Set") {
+    val a = randomString
+    val b = randomString
+    val c = randomString
+    val d = randomString
+    val e = randomString
+
+    val left = SetOop.empty.add(a).add(b).add(c)
+    val right = SetOop.empty.add(c).add(d).add(e)
+
+    // As we have the equality function, so we can test using equality function
+    /*
+    val intersectionLeft = left.intersect(right)
+
+    intersectionLeft(a) shouldBe false
+    intersectionLeft(b) shouldBe false
+    intersectionLeft(c) shouldBe true
+    intersectionLeft(d) shouldBe false
+    intersectionLeft(e) shouldBe false
+
+    val intersectionRight = right.intersect(left)
+
+    intersectionRight(a) shouldBe false
+    intersectionRight(b) shouldBe false
+    intersectionRight(c) shouldBe true
+    intersectionRight(d) shouldBe false
+    intersectionRight(e) shouldBe false
+    */
+
+    // As we have the equality function, so we can test the above using equality function
+    // Test with equality on Set
+    left.intersect(right) shouldBe SetOop.empty.add(c)
+    right.intersect(left) shouldBe SetOop.empty.add(c)
+  }
+
+  // Test difference element of Set
+  test("difference on empty Set should yield an empty Set") {
+    SetOop.empty.diff(SetOop.empty)(randomString) shouldBe false
+  }
+
+  test("difference on a non empty Set with an empty Set should yield an empty Set") {
+    val first = randomString
+    val second = randomString
+
+    first should not be second
+
+    val emptySet = SetOop.empty
+    val nonEmptySet = emptySet.add(first).add(second)
+
+    emptySet.diff(nonEmptySet)(first) shouldBe false
+    emptySet.diff(nonEmptySet)(second) shouldBe false
+
+    nonEmptySet.diff(emptySet)(first) shouldBe true
+    nonEmptySet.diff(emptySet)(second) shouldBe true
+  }
+
+  test("difference on two non empty Sets should yield their difference") {
+    val a = randomString
+    val b = randomString
+    val c = randomString
+    val d = randomString
+
+    val left = SetOop.empty.add(a).add(b).add(c)
+    val right = SetOop.empty.add(b).add(c).add(d)
+  
+    // As we have the equality function, so we can test using equality function
+    /* 
+    val leftDifference = left.diff(right)
+
+    leftDifference(a) shouldBe true
+    leftDifference(b) shouldBe false
+    leftDifference(c) shouldBe false
+    leftDifference(d) shouldBe false
+
+    val rightDifference = right.diff(left)
+
+    rightDifference(a) shouldBe false
+    rightDifference(b) shouldBe false
+    rightDifference(c) shouldBe false
+    rightDifference(d) shouldBe true
+     */
+
+     // As we have the equality function, so we can test the above using equality function
+    left.diff(right) shouldBe SetOop.empty.add(a)
+    right.diff(left) shouldBe SetOop.empty.add(d)
+
+  }
+
+  // Test subset property of Set
+  test("isSubsetOf on an empty Set should yield true") {
+    // pending
+    SetOop.empty.isSubsetOf(SetOop.empty) shouldBe true
+    SetOop.empty.isSubsetOf(SetOop.empty.add(randomString)) shouldBe true
+  }
+
+  test("isSubsetOf on itself should yield true") {
+    // pending
+    val set = SetOop.empty.add(randomString)
+    set.isSubsetOf(set) shouldBe true    
+  }
+
+  test("isSubsetOf on a non empty set should yield false") {
+    val a = randomString
+    val b = randomString
+    val c = randomString
+
+    val left = SetOop.empty.add(a).add(b)
+    val right = left.add(c)
+
+    left.isSubsetOf(right) shouldBe true
+    right.isSubsetOf(left) shouldBe false
+  }
+
+  // Test superset property of Set
+  test("isSupersetOf on an empty Set should yield true") {
+    // pending
+    SetOop.empty.isSupersetOf(SetOop.empty) shouldBe true
+    SetOop.empty.add(randomString).isSupersetOf(SetOop.empty) shouldBe true
+    SetOop.empty.isSupersetOf(SetOop.empty.add(randomString)) shouldBe false
+  }
+
+  test("isSupersetOf on itself should yield true") {
+    // pending
+    val set = SetOop.empty.add(randomString)
+    set.isSupersetOf(set) shouldBe true    
+  }
+
+  test("isSupersetOf on a non empty set should yield false") {
+    val a = randomString
+    val b = randomString
+    val c = randomString
+
+    val left = SetOop.empty.add(a).add(b)
+    val right = left.add(c)
+
+    left.isSupersetOf(right) shouldBe false
+    right.isSupersetOf(left) shouldBe true
+  }
+
+  // ** As we're using `case class`, equality & hashCode are free from `case class`
+  // ** i.e. equality & hashCode are generated from `case class`
+  // Test hashcode in Set
+  test("hashCode on an empty Set should not be random"){
+    SetOop.empty.hashCode shouldBe SetOop.empty.hashCode
+    val element = randomString
+    SetOop.empty.add(element).hashCode shouldBe SetOop.empty.add(element).hashCode
+  }
+
+  test("hashCode on an empty Set should not be 0") {
+    SetOop.empty.hashCode should not be 0
+  }
+
+  // Test of size of Set
+  test("hashCode on a non empty Set should be the sum of all the hashCodes and the hashCode of the empty Set") {
+    val first = randomString
+    val second = randomString
+    
+    val expected = SetOop.empty.hashCode + first.hashCode + second.hashCode
+    
+    SetOop.empty.add(first).add(second).hashCode shouldBe expected
+  }
+
+  test("size on an empty Set should be 0") {
+    SetOop.empty.size shouldBe 0
+  }
+
+  test("size on a non empty Set should be 1") {
+    SetOop.empty.add(randomString).size shouldBe 1
+  }
+
+  test("size on a non empty Set with 2 distinct elements added should be 2") {
+    val first = randomString
+    val second = randomString
+
+    first should not be second
+
+    SetOop.empty.add(first).add(second).size shouldBe 2
+  }
+
+  test("size on a non empty Set with 2 equal elements added should be 1") {
+    val element = randomString
+
+    SetOop.empty.add(element).add(element).size shouldBe 1
+  }
+
+  // Test empty of Set
+  test("isEmpty on an empty Set should yield true") {
+    SetOop.empty.isEmpty shouldBe true
+    SetOop.empty.nonEmpty shouldBe false
+  }
+
+  test("isEmpty on a non empty Set should yield false") {
+    SetOop.empty.add(randomString).isEmpty shouldBe false
+    SetOop.empty.add(randomString).nonEmpty shouldBe true
+  }
+
+  // Test isSingleton on Set
+  test("isSingleton on an empty Set should yield false") {
+    SetOop.empty.isSingleton shouldBe false
+  }
+
+  test("isSingleton on a Set with more than one element should yield false") {
+    val first = randomString
+    val second = randomString
+
+    first should not be second
+
+    SetOop.empty.add(first).add(second).isSingleton shouldBe false
+  }
+
+  test("isSingleton on a Set with a single element should yield true") {
+    SetOop.empty.add(randomString).isSingleton shouldBe true
+  }
+
+  test("sample should yield a random element from the Set") {
+    SetOop.empty.sample shouldBe None
+
+    val a = randomString
+    SetOop.empty.add(a).sample shouldBe Some(a)
+
+    val b = randomString
+    SetOop.empty.add(a).add(b).sample should contain oneOf (a, b)
+  }
+
 }
